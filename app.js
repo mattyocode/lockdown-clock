1235b6f50f060aeab04e38111388b3449228ee32
mattyocode <matthewoliver@live.co.uk>
2021-03-04 10:35:31 +0000

    fix: remove white space under carousel on mobile

--- app.js
+++ app.js
@@ -1,5 +1,75 @@
+function responsiveCarousel(){
+
+  const carouselSlide = document.querySelector('.carousel-slide');
+  const carouselContent = document.querySelectorAll('.full-bleed');
+
+  // Buttons
+  const prevBtn = document.getElementById('prev');
+  const nextBtn = document.getElementById('next');
+
+  // Counter
+  let counter = 0;
+  let width = carouselContent[0].clientWidth;
+
+  window.addEventListener('resize', () => {
+      width = carouselContent[counter].clientWidth;
+      // carouselContent.forEach((item)=>{
+      //     item.style.width = width;
+      // })
+      carouselSlide.style.transform = 'translateX(' + (-width * counter) + 'px)';
+  });
+
+
+
+  carouselSlide.style.transform = 'translateX(' + (-width * counter) + 'px)';
+
+  // Button listeners
+
+  function counterInRange(){
+      return (counter <= 0 || counter >= carouselContent.length - 1);
+  };
+
+  function checkBtnDisplay(){
+      if(counter <= 0){
+          prevBtn.classList.add('hide');
+          nextBtn.classList.remove('hide');
+      } else if(counter >= carouselContent.length - 1){
+          prevBtn.classList.remove('hide');
+          nextBtn.classList.add('hide');
+      } else {
+          nextBtn.classList.remove('hide');
+          prevBtn.classList.remove('hide');
+      };
+  };        
+
+
+  nextBtn.addEventListener('click', () => {
+      if(counter >= carouselContent.length - 1) return;
+      carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+      counter++;
+      carouselSlide.style.transform = 'translateX(' + (-width * counter) + 'px)';
+  });
+
+  prevBtn.addEventListener('click', () => {
+      if(counter <= 0) return;
+      carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+      counter--;
+      carouselSlide.style.transform = 'translateX(' + (-width * counter) + 'px)';
+  });
+
+  window.addEventListener('DOMContentLoaded', () => {
+      checkBtnDisplay();
+  });
+
+  carouselSlide.addEventListener('transitionend', () => {
+      checkBtnDisplay();
+  });
+};
+
+responsiveCarousel();
+
 function typingEffect(){
-  const texts = ['Fewer Zoom Meetings', 'End of Lockdown', 'The Actual Pub', 'Seeing Your Pals', 'Seeing Your Fam', 'Proper Holidays'];
+  const texts = ['end of lockdown', 'hugging your fam', "you're not on mute", 'the indoors pub', 'seeing your pals', 'proper holidays', 'the gym'];
   let count = 0;
   let charIndex = 0;
   let currentText = '';
@@ -18,6 +88,8 @@ function typingEffect(){
       count++;
       charIndex = 0;
     }
+
+
 
     let interval = Math.floor(Math.random() * 200) + 100
 
@@ -114,7 +186,7 @@ function setCountdown(countdownId, futureDate){
   getRemainingTime();
 };
 
-typingEffect();
+// typingEffect();
 
 // set first clock
 let stepOneDate = new Date(2021,2,8,0,0,1);
